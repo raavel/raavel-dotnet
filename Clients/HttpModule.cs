aaf22ee17b45857fa610c41c9a0e48855c8d4a11
Mitesh Patel <m.patel@jobtarget.com>
2016-04-18 15:44:28 -0400

    Added Http Reporting Module

---
+++ Clients/HttpModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Raavel.Clients
+{
+    class HttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.Error += SendError;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private static void SendError(object sender, EventArgs e)
+        {
+            var application = (HttpApplication)sender;
+            var ex = application.Server.GetLastError();
+
+            if (ex is HttpUnhandledException)
+                ex = ex.InnerException;
+
+            new BaseClient().Notify(ex);
+        }
+    }
+}
